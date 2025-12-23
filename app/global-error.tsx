77018fef96378fe296a40d8e02dc4b65b5a2f083
vatsal-pytheta <vatsal@pytheta.com>
2025-12-23 14:59:47 +0530

    Project setup

---
+++ app/global-error.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { captureException } from "@sentry/nextjs";
+import NextError from "next/error";
+import { useEffect } from "react";
+
+export default function GlobalError({ error }: Readonly<{ error: Error & { digest?: string } }>) {
+  useEffect(() => {
+    captureException(error);
+  }, [error]);
+
+  return (
+    <html lang="en">
+      <body>
+        <NextError statusCode={0} />
+      </body>
+    </html>
+  );
+}
